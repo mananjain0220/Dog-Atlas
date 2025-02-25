51b06f2b985213b7e1a7ee188e11dd2488416ee9
mananjain0220 <mananjain0220@gmail.com>
2025-02-25 12:11:25 +0100

    Initial Commit

---
+++ DogApp/DogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogAppApp.swift
+//  DogApp
+//
+//  Created by manan jain on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct DogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
